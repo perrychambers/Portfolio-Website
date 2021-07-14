2f22b2e8bcfc6c119e8d1b7932a751c164ad8c87
perry <pkc5102@psu.edu>
2021-07-14 19:10:06 -0400

    fixed sizing on project cards

--- website/src/components/Projects/ProjectsElements.js
+++ website/src/components/Projects/ProjectsElements.js
@@ -18,7 +18,7 @@ export const ProjectsContainer = styled.div`
 `
 
 export const ProjectsWrapper = styled.div`
-    max-width: 800px;
+    max-width: 1000px;
     margin: 0 auto;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
