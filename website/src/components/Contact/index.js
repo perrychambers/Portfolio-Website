37b357da1e766a5d2cd57976c1484bc3f7872f0f
perry <pkc5102@psu.edu>
2021-07-19 19:21:50 -0400

    Added copy email to clipboard functionality.

--- website/src/components/Contact/index.js
+++ website/src/components/Contact/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import {Container, FormWrap, Icon, FormContent, Form, FormH1, FormInput, FormLabel} from './ContactElements'
+import {Container, FormWrap, Icon, FormContent, Form, FormH1 } from './ContactElements'
 import { Button } from '../ButtonElement'
 
 const Contact = ({id, primary, dark}) => {
 
+const copyEmail = () => {
+    var text = 'pkc5102@psu.edu';
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Email copied to clipboard.')
+    }, function(err) {
+        console.error('Could not copy to clipboard.', err);
+    });
+}
 
     return (
         <Container id={id}>
@@ -11,13 +19,13 @@ const Contact = ({id, primary, dark}) => {
                 <Icon to='/'></Icon>
                 <FormContent>
                     <Form action='#'>
-                        <FormH1>Contact Me</FormH1>
-                        <FormLabel htmlFor='for'>Name</FormLabel>
+                        <FormH1>Get in contact with me!</FormH1>
+                        {/* <FormLabel htmlFor='for'>Name</FormLabel>
                         <FormInput type='name' required />
                         <FormLabel htmlFor='for'>Email</FormLabel>
                         <FormInput type='email' required />
                         <FormLabel htmlFor='for'>Your Message</FormLabel>
-                        <FormInput type='message' required />
+                        <FormInput type='message' required /> */}
                         <Button to='home'
                             smooth={true}
                             duration={500}
@@ -25,9 +33,9 @@ const Contact = ({id, primary, dark}) => {
                             exact='true'
                             offset={-80}
                             primary={primary ? 1 : 0 }
-                            dark={dark ? 1 : 0}
-                            onClick={() => { alert('Section under development.')}}
-                        >Submit</Button>
+                            dark={dark ? 1 : 0} 
+                            onClick={copyEmail}
+                        >Send me an email.</Button>
                     </Form>
                 </FormContent>
             </FormWrap>
